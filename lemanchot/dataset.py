623e282dd716338289453dfe07fed9a5f8cde232
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2022-09-07 03:11:00 -0400

    implement Huang2020Dataset

--- lemanchot/dataset.py
+++ lemanchot/dataset.py
@@ -7,9 +7,12 @@
     @email      parham.nooralishahi@gmail.com
 """
 
+import glob
 import os
+import re
 import numpy as np
 
+from PIL import Image
 from typing import Dict
 from torch.utils.data import Dataset
 from gimp_labeling_converter import XCFDataset, generate_cmap, gimp_helper
@@ -67,3 +70,57 @@ class RepetitiveDatasetWrapper(XCFDataset):
     def __getitem__(self, idx):
         super().__getitem__(idx % self.actual_size)
 
+class Huang2020Dataset(Dataset):
+    """ Dataset class handling the texture dataset presented by Huang et. al (2020)
+        Citation: Huang, Yibin, Congying Qiu, Xiaonan Wang, Shijun Wang, and Kui Yuan. 
+                  "A compact convolutional neural network for surface defect inspection." 
+                  Sensors 20, no. 7 (2020)
+        Dataset: https://github.com/abin24/Textures-Dataset
+    Args:
+        root_dir (str): directory path of dataset in the local storage
+        categories (Dict[str,int]): the list of categories and associated label indexes
+        transform (_type_): the transformation needed to be applied on the given data
+        target_transform (_type_): the target transformation needed to be applied on the given target
+    """
+
+    def __init__(self,
+        root_dir : str,
+        categories: Dict[str, int], 
+        transform=None, 
+        target_transform=None
+    ) -> None:
+        super().__init__()
+        self.transform = transform
+        self.target_transform = target_transform
+        self.categories = categories
+        self.root_dir = root_dir
+        self._dataset = []
+        # Extract list of categories
+        subdir = os.listdir(root_dir)
+        for cname in subdir:
+            # Extract category
+            category = cname.split('_')
+            if len(category) < 2:
+                continue
+            category = re.search(r'\D+', category[-1]).group(0)
+            # Skip the category folder
+            if not category in categories.keys():
+                continue
+            # Generate the 
+            files = glob.glob(os.path.join(root_dir, cname, '*.jpg'))
+            for f in files:
+                data = (f, category, categories[category])
+                self._dataset.append(data)
+    
+    def __len__(self):
+        return len(self._dataset)
+    
+    def __getitem__(self, idx):
+        data = self._dataset[idx]
+        # Load image as a numpy array
+        img = np.asarray(Image.open(data[0]))
+        # Generate the target image
+        target_classid = data[-1]
+        target_shape = img.shape if len(img.shape) < 3 else img.shape[:-1]
+        target = np.full(target_shape, target_classid)
+        return (img, target)
--- test/test_model.py
+++ test/test_model.py
@@ -3,12 +3,39 @@
 import os
 import sys
 import unittest
+import torch
+import numpy as np
+
+from torch.utils.data import DataLoader
 
 sys.path.append(os.getcwd())
 sys.path.append(__file__)
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-class TestModel(unittest.TestCase):
+from lemanchot.dataset import Huang2020Dataset
+
+class TestDataset(unittest.TestCase):
     
-    def test_model(self):
-        pass
+    def test_Huang2020Dataset(self):
+        dataset = Huang2020Dataset(
+            root_dir = '/home/phm/Datasets/texture/train',
+            categories={
+                'linen' : 1,
+                'styrofoam' : 2,
+                'ceiling' : 3,
+                'sand' : 4,
+                'stone' : 5,
+                'wood' : 6,
+                'water' : 7,
+                'wall' : 8
+            }
+        )
+        batch_size = 2
+        data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        # data = next(iter(data_loader))
+        for img, target in data_loader:
+            assert len(torch.unique(target)) <= batch_size
+    
+
+if __name__ == '__main__':
+    unittest.main()
