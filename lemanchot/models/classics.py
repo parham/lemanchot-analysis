e6d36dd6773c8de92f1a1a20458a2b877752fd35
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2022-09-09 19:52:38 -0400

    implement dbscan

---
+++ lemanchot/models/classics.py
@@ -0,0 +1,40 @@
+
+""" 
+    @title A Deep Semi-supervised Segmentation Approach for Thermographic Analysis of Industrial Components
+    @organization Laval University
+    @professor  Professor Xavier Maldague
+    @author     Parham Nooralishahi
+    @email      parham.nooralishahi@gmail.com
+"""
+
+import numpy as np
+from dotmap import DotMap
+from sklearn.cluster import DBSCAN, KMeans, MeanShift, estimate_bandwidth
+
+from lemanchot.models.core import model_register, BaseModule
+
+
+@model_register('dbscan')
+class UnetPlusPlus_Resnet18(BaseModule):
+    def __init__(self, name : str, config : DotMap) -> None:
+        super().__init__(
+            name='dbscan',
+            config=config
+        )
+        self.clss = DBSCAN(
+            eps = self.eps,
+            min_samples = self.min_samples,
+            leaf_size = self.leaf_size
+        )
+    
+    def forward(self, inputs):
+        batch_size = inputs.shape[0]
+        outputs = []
+        for bindex in range(batch_size):
+            input = inputs.take(indices=bindex, axis=0)
+            input_size = input.shape
+            db = self.clss.fit(inputs)
+            output = np.uint8(db.labels_.reshape(input_size[:2]))
+            outputs.append(output)
+        outputs = np.dstack(outputs)
+        return outputs
---
+++ lemanchot/pipeline/classics.py
@@ -0,0 +1,56 @@
+
+""" 
+    @title A Deep Semi-supervised Segmentation Approach for Thermographic Analysis of Industrial Components
+    @organization Laval University
+    @professor  Professor Xavier Maldague
+    @author     Parham Nooralishahi
+    @email      parham.nooralishahi@gmail.com
+"""
+
+from time import time
+from typing import Dict
+from ignite.engine import Engine
+
+import torch
+import torch.optim as optim
+
+import numpy as np
+from sklearn.cluster import DBSCAN, KMeans, MeanShift, estimate_bandwidth
+
+from comet_ml import Experiment
+
+from lemanchot.pipeline.core import pipeline_register
+from lemanchot.models import BaseModule
+
+@pipeline_register("dbscan_train")
+def dbscan_train_step__(
+    engine : Engine, 
+    batch,
+    device,
+    model : BaseModule,
+    criterion,
+    optimizer : optim.Optimizer,
+    experiment : Experiment
+) -> Dict:
+    inputs, targets = batch
+
+    t = time.time()
+
+    inputs = inputs.cpu().detach().numpy()
+    targets = targets.cpu().detach().numpy()
+    batch_size = inputs.shape[0]
+
+    criterion.prepare_loss(ref=batch[0])
+
+    outputs = model(inputs)
+
+    outputs = torch.tensor(torch.argmax(outputs, dim=1), dtype=targets.dtype, requires_grad=True)
+    targets = targets.squeeze(1)
+    loss = criterion(outputs, targets)
+
+    return {
+        'y' : targets,
+        'y_pred' : outputs,
+        'loss' : loss.item()
+    }
+
