2cdcc1b1464799771d89b4fbdbad258164b514ed
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2022-09-13 23:36:29 -0400

    implement image dataset

--- lemanchot/dataset.py
+++ lemanchot/dataset.py
@@ -7,9 +7,12 @@
     @industrial-partner TORNGATS
 """
 
+import functools
+import glob
 import os
 import numpy as np
 
+from PIL import Image
 from typing import Dict
 from torch.utils.data import Dataset
 from gimp_labeling_converter import XCFDataset, generate_cmap, gimp_helper
@@ -67,3 +70,35 @@ class RepetitiveDatasetWrapper(XCFDataset):
     def __getitem__(self, idx):
         super().__getitem__(idx % self.actual_size)
 
+
+class ImageDataset(Dataset):
+    """
+    Dataset for images in a folder
+    """
+    def __init__(self,
+        root_dir : str,
+        file_extension : str,
+        transform=None, 
+        target_transform=None
+    ) -> None:
+        """
+        Args:
+            root_dir (str): root directory containing images
+            file_extension (str): the targeted file extension
+            transform (_type_, optional): the transformations for the images. Defaults to None.
+            target_transform (_type_, optional): the transformations for the targets. Defaults to None.
+        """ 
+        super().__init__()
+        self.transform = transform
+        self.target_transform = target_transform
+        self.root_dir = root_dir
+        self.file_list = glob.glob(os.path.join(self.root_dir, f'*.{file_extension}'))
+
+    def __len__(self):
+        return len(self.file_list)
+    
+    @functools.lru_cache(maxsize=10)
+    def __getitem__(self, idx):
+        fs = self.file_list[idx]
+        img = np.asarray(Image.open(fs))
+        return (img, None)
