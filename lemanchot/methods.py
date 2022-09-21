f2358a6ed0fb9664c1f509da5eefd49de036ee80
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2022-09-20 21:56:35 -0400

    partial implementation

---
+++ lemanchot/methods.py
@@ -0,0 +1,70 @@
+
+""" 
+    @title A Deep Semi-supervised Segmentation Approach for Thermographic Analysis of Industrial Components
+    @organization Laval University
+    @professor  Professor Xavier Maldague
+    @author     Parham Nooralishahi
+    @email      parham.nooralishahi@gmail.com
+"""
+
+import logging
+import torch
+
+from lemanchot.core import get_device
+from lemanchot.loss.core import load_loss
+from lemanchot.models.core import load_model
+from lemanchot.pipeline.core import load_optimizer
+
+
+def iterative_region_segmentation(
+    batch,
+    experiment_config,
+    transforms,
+    device,
+    num_iteration : int,
+    class_count_limit : int
+):
+    input = batch[0]
+    input = input.to(dtype=torch.float32, device=device)
+    # Create model instance
+    logging.info('Loading model ...')
+    model = load_model(experiment_config)
+    model.to(device)
+    # Create loss instance
+    logging.info('Loading loss ...')
+    criterion = load_loss(experiment_config)
+    criterion.to(device)
+    # Create optimizer instance
+    logging.info('Loading optimizer ...')
+    optimizer = load_optimizer(model, experiment_config)
+    # Create transformations
+    logging.info('Creating and Applying transformations ...')
+    # Apply the transformations to the given data
+    input = transforms(input)
+    input = input.to(dtype=torch.float32, device=device)
+    # Prepare the loss function for the pipeline
+    criterion.prepare_loss(ref=input)
+
+    result = None
+    for iter in range(num_iteration):
+        # Set the model into the training mode
+        model.train()
+        optimizer.zero_grad()
+        # Pass the data to the model
+        output = model(input.unsqueeze(0))
+        output = output.squeeze(0)
+        # Calculate the output as the new target
+        _, trg = torch.max(output, 0)
+        loss = criterion(output, trg)
+        trg = trg.unsqueeze(0).unsqueeze(0).to(dtype=torch.uint8)
+
+        loss.backward()
+        optimizer.step()
+        # Calculate the number of calculated classes
+        num_classes = len(torch.unique(trg))
+        # Check that the number of classes is not less than the defined limit
+        if num_classes <= class_count_limit:
+            break
+
+        output = trg.squeeze(0).squeeze(0)
+        yield output
--- use_cases/piping_inspection/thermal_segmentation.py
+++ use_cases/piping_inspection/thermal_segmentation.py
@@ -1,6 +1,6 @@
 
 """ 
-    @title A Deep Semi-Supervised Segmentation Approach for Thermographic Analysis of Industrial Components
+    @title A Deep Semi-supervised Segmentation Approach for Thermographic Analysis of Industrial Components
     @organization Laval University
     @professor  Professor Xavier Maldague
     @author     Parham Nooralishahi
@@ -8,20 +8,17 @@
 """
 
 import argparse
+import logging
 import os
 import sys
-import logging
-from typing import List
-import numpy as np
 
-import cv2
-from PIL import Image
-from pathlib import Path
 from tqdm import trange
-from scipy.io import savemat, loadmat
 
-import torch
 import torchvision.transforms as transforms
+
+sys.path.append(os.getcwd())
+sys.path.append(__file__)
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from lemanchot.core import get_config, get_device
 from lemanchot.loss.core import load_loss
@@ -47,25 +44,18 @@ parser.add_argument('--iteration', type=int, default=60,
                     help="The maximum number of iteration.")
 parser.add_argument('--nclass', type=int, default=10,
                     help="The minimum number of classes.")
-# parser.add_argument('--output', type=str, default='.', help="The result folder.")
-# parser.add_argument('--registeration', action='store_false', help="Enable the multi-modal registration feature.")
 
-def region_segmentation():
+def main():
     args = parser.parse_intermixed_args()
     parser.print_help()
 
     if not os.path.isfile(args.file):
         logging.error(f'{args.file} does not exist!')
         return
-
+    
+    # Determine the selected device to use for processing
     device = get_device()
-
-    data = loadmat(args.file)
-    thermal = data['aligned_ir'] if 'aligned_ir' in data else None
-    thermal_roi = data['ir_roi'] if 'ir_roi' in data else None
-    visible = data['viz'] if 'viz' in data else None
-    visible_roi = data['viz_roi'] if 'viz_roi' in data else None
-
+    # Load the experiment configuration
     experiment_config = get_config('wonjik2020')
     # Create model instance
     logging.info('Loading model ...')
@@ -87,39 +77,16 @@ def region_segmentation():
         FilterOutAlphaChannel(),
         ToFloatTensor()
     ])
-    # Apply the transformations to the given data
-    input = transform(thermal_roi)
-    input = input.to(dtype=torch.float32, device=device)
 
-    criterion.prepare_loss(ref=input)
+    # Create the dataset
+    
 
-    result = None
     for iter in trange(args.iteration):
-        model.train()
-        optimizer.zero_grad()
 
-        output = model(input.unsqueeze(0))
-        output = output.squeeze(0)
-
-        _, trg = torch.max(output, 0)
-        loss = criterion(output, trg)
-        trg = trg.unsqueeze(0).unsqueeze(0).to(dtype=torch.uint8)
-
-        loss.backward()
-        optimizer.step()
-
-        num_classes = len(torch.unique(trg))
-        if num_classes <= args.nclass:
-            break
-
-        result = trg
-
-    logging.info('Saving the result ... ')
-    thermal_seg = result.squeeze(0).squeeze(0)
-    data['ir_seg'] = thermal_seg.cpu().detach().numpy()
-
-    savemat(args.file, data, do_compression=True)
 
 
 if __name__ == "__main__":
-    region_segmentation()
+    try:
+        main()
+    except Exception as ex:
+        logging.exception(ex)
